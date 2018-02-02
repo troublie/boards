5f133c00660b4d28ae06298029ef4eb368ce757f
Juliano Moraes Lopes <juliano.lopes@FujidoBrasil.local>
2018-02-02 17:07:05 -0200

    passo5

--- boards/views.py
+++ boards/views.py
@@ -5,3 +5,8 @@ from .models import Board
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+
+
+def board_topics(request, pk):
+    board = Board.objects.get(pk=pk)  # recupera o board da DB com o a PK
+    return render(request, 'topics.html', {'board': board})
--- myproject/urls.py
+++ myproject/urls.py
@@ -21,5 +21,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     path('admin/', admin.site.urls),
 ]

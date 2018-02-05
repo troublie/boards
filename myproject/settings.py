7b1687a4b2f2e593815cf37fba4bc376264a9978
Juliano Moraes Lopes <juliano.lopes@FujidoBrasil.local>
2018-02-05 15:07:01 -0200

    parte3.2 forms widget tweaks

--- myproject/settings.py
+++ myproject/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'boards',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- myproject/urls.py
+++ myproject/urls.py
@@ -22,5 +22,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
 ]

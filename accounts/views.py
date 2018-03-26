0b99a9c06d2a3679d9ac6751ebaf5c563757059c
Juliano <juliano.lopes@mackenzista.com.br>
2018-03-26 16:19:20 -0300

    Sign up raw OK

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import \
+    UserCreationForm  # aqui importamos o django battery included form de User, o qual utiliza o model user dele mesmo também
+
 
 # Create your views here.
 
 def signup(request):
-    return render(request, 'signup.html')
+    form = UserCreationForm() #instanciamos a UserCreationForm na variável form
+    return render(request, 'signup.html', {'form': form}) #retornamos o renderizador com os objetos respectivos
+
+#daqui voltamos para o signup.html

cb3655b011fc1605916dbcf667ecde29c5a1ad61
Juliano <juliano.lopes@mackenzista.com.br>
2018-03-26 15:04:35 -0300

    Parte 3 completa

--- boards/admin.py
+++ boards/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Board
+from .models import Topic
 
 admin.site.register(Board)
+admin.site.register(Topic)
--- boards/models.py
+++ boards/models.py
@@ -21,4 +21,4 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, null=True, on_delete = models.CASCADE, related_name='posts')
-    updated_by = models.ForeignKey(User, on_delete = models.CASCADE, null=True, related_name='+')
+    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete = models.CASCADE)

3e1a4ed80236587c5cf29ec9ac7e225444103093
Juliano <juliano.lopes@mackenzista.com.br>
2018-03-26 17:44:27 -0300

    User form OK, bootstrap nome usuario OK, a implementar testes

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import \
     UserCreationForm  # aqui importamos o django battery included form de User, o qual utiliza o model user dele mesmo também
+from django.contrib.auth import login as auth_login  # novamente o AS é para criar um alias.
 
 
 # Create your views here.
 
 def signup(request):
-    form = UserCreationForm() #instanciamos a UserCreationForm na variável form
-    return render(request, 'signup.html', {'form': form}) #retornamos o renderizador com os objetos respectivos
+    if request.method == 'POST':  # aqui validamos se é um POST ou GET (post seria forms enviadas e etc)
+        form = UserCreationForm(request.POST)  # aqui atribuimos os dados recebidos a variavel form
+        if form.is_valid():  # valida os dados da form
+            user = form.save()  # cria um User, não salva no DB
+            auth_login(request, user)  # não sei, a adicionar // edit: Aqui o user criado é autenticado
+            return redirect('home')  # redireciona para home após cadastro OK e user autenticado
+    else:
+        form = UserCreationForm()  # instanciamos a UserCreationForm na variável form SE FOR GET (CONFORME IF LA EM CIMA)
+    return render(request, 'signup.html', {'form': form})  # retornamos o renderizador com os objetos respectivos
 
-#daqui voltamos para o signup.html
+# daqui voltamos para o signup.html
